71ec58d89325057b0b92079b7a7d7bb832aa7779
Susara Thathsarani <susarathathsarani3@gmail.com>
2025-10-04 15:39:54 +0530

    Add CI workflow and project files

---
+++ src/test/java/com/QA/demo/selenium/UITest.java
@@ -0,0 +1,66 @@
+package com.QA.demo.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UITest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testLoginFormElementsPresent() {
+        driver.get("file:///D:/Quality_Testing-master/src/test/resources/ui/index.html");
+
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.tagName("button"));
+
+        assertTrue(username.isDisplayed());
+        assertTrue(password.isDisplayed());
+        assertEquals("Login", loginButton.getText());
+    }
+
+    @Test
+    public void testAddItem() {
+        driver.get("file:///D:/Quality_Testing-master/src/test/resources/ui/index.html");
+
+        WebElement input = driver.findElement(By.id("itemInput"));
+        WebElement addButton = driver.findElement(By.id("addItemBtn"));
+
+        input.sendKeys("Test Item");
+        addButton.click();
+
+        new WebDriverWait(driver, Duration.ofSeconds(2))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul[@id='itemList']/li")));
+
+        WebElement listItem = driver.findElement(By.xpath("//ul[@id='itemList']/li"));
+        System.out.println("List item text: " + listItem.getText());
+
+        assertEquals("Test Item", listItem.getText());
+    }
+}
